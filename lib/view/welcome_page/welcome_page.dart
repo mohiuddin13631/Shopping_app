80ef6412f6a2784f544bdd877f165a419191d710
Cse-Akash-Khan <cseakash.evening@gmail.com>
2022-05-08 00:23:05 +0600

    set backgroud

--- lib/view/welcome_page/welcome_page.dart
+++ lib/view/welcome_page/welcome_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'components/background.dart';
+
 class welcomePage extends StatelessWidget {
   const welcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Welcome to shopping app")),
+    return Scaffold(
+      body: Background(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Text("Wellcome to shop family", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+          ],)
+      )
     );
   }
 }
