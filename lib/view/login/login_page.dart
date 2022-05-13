c7abefbbee8d9bf71587761bcfbd0861e9cccecc
Cse-Akash-Khan <cseakash.evening@gmail.com>
2022-05-14 02:23:31 +0600

    Login page completed password visibility feature added

--- lib/view/login/login_page.dart
+++ lib/view/login/login_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/view/custom_widget/my_theme.dart';
 import 'package:shopping_app/view/login/components/login_background.dart';
+import 'package:shopping_app/view/login/components/password_field.dart';
 import 'package:shopping_app/view/login/components/textfield_decorator.dart';
+import 'package:shopping_app/view/signup/signup.dart';
+import 'package:shopping_app/view/welcome_page/components/custom_button.dart';
 
 import 'components/userid_text_field.dart';
 
@@ -38,7 +41,51 @@ class LoginPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFieldDecorator(
-                  child: UserIdTextField(useridcontroller: useridcontroller, userIdErrorText: userIdErrorText, userIdHintText: userIdHintText, userIdHintTextColor: userIdHintTextColor, userIdTextFieldPrefixIcon: userIdTextFieldPrefixIcon, userIdTextFieldPrefixIconColor: userIdTextFieldPrefixIconColor, onUserIdValueChange: (value){},),
+                  child: UserIdTextField(
+                    useridcontroller: useridcontroller,
+                    userIdErrorText: userIdErrorText,
+                    userIdHintText: userIdHintText,
+                    userIdHintTextColor: userIdHintTextColor,
+                    userIdTextFieldPrefixIcon: userIdTextFieldPrefixIcon,
+                    userIdTextFieldPrefixIconColor:
+                        userIdTextFieldPrefixIconColor,
+                    onUserIdValueChange: (value) {},
+                  ),
+                ),
+                TextFieldDecorator(
+                    child: UserPassTextField(
+                  userPasscontroller: passcontroller,
+                  userPassErrorText: "Password can not be empty",
+                  userPassHintText: "Enter Password",
+                  userPassHintTextColor: Colors.purple,
+                  userPassTextFieldPrefixIcon: Icons.lock,
+                  userPassTextFieldPrefixIconColor: Colors.purple,
+                  suffixIcon: Icons.visibility_off,
+                  suffixIconColor: Colors.purple,
+                  onUserPassValueChange: (value) {},
+                )),
+                CustomButton(
+                    buttonColor: MyTheme.loginButtonColor,
+                    buttontext: "LOGIN",
+                    textColor: Colors.white,
+                    handleButtonClick: (){print("login");}
+                    ),
+                const SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have account ?",style: TextStyle(fontWeight: FontWeight.bold),),
+                    const SizedBox(width: 10,),
+                    InkWell(
+                        child: const Text(
+                            "Sign Up",
+                            style: TextStyle(fontWeight: FontWeight.bold,color: Colors.purple),
+                        ),
+                      onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUp()));
+                      },
+                    )
+                  ],
                 )
               ],
             ),
@@ -48,4 +95,3 @@ class LoginPage extends StatelessWidget {
     )));
   }
 }
-
